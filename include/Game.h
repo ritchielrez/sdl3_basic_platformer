222430b636751b3be2aa5748fb48d5aef4ae5bae
RitchielRez <ritchielrez@gmail.com>
2026-02-18 14:30:14 -0500

    refactor: move `createPlayer()` and `loadTileMap()` to `Game` class

--- include/Game.h
+++ include/Game.h
@@ -6,6 +6,8 @@
 
 #include "DynTile.h"
 #include "Player.h"
+#include "ResourceManager.h"
+#include "SDLState.h"
 #include "StaticTile.h"
 
 namespace Tiles {
@@ -30,4 +32,89 @@ struct Game {
   static inline Player player{};
   static inline std::vector<StaticTile> staticTiles{};
   static inline std::vector<DynTile> dynTiles{};
+
+  static inline void createPlayer(const SDLState &sdlState,
+                                  const ResourceManager &resourceManager) {
+    constexpr size_t PLAYER_IDLE_FRAMES = 4;
+    constexpr size_t PLAYER_RUN_FRAMES = 16;
+    constexpr float PLAYER_SIZE = 32.0f;
+
+    std::vector<Frames> playerAnims;
+    playerAnims.resize(5);
+
+    playerAnims[PlayerAnim::idle] = Frames(
+        PLAYER_IDLE_FRAMES, 0.5f,
+        std::vector<glm::vec2>{
+            glm::vec2(0 * PLAYER_SIZE, 0.0f), glm::vec2(1 * PLAYER_SIZE, 0.0f),
+            glm::vec2(2 * PLAYER_SIZE, 0.0f), glm::vec2(3 * PLAYER_SIZE, 0.0f)},
+        PLAYER_SIZE, PLAYER_SIZE);
+
+    std::vector<glm::vec2> playerRunTexCoords{PLAYER_RUN_FRAMES};
+    for (size_t i = 0; i < PLAYER_RUN_FRAMES; i++) {
+      if (i <= 7) {
+        playerRunTexCoords[i].x = i * PLAYER_SIZE;
+        playerRunTexCoords[i].y = 2 * PLAYER_SIZE;
+      } else {
+        playerRunTexCoords[i].x = (i - 8) * PLAYER_SIZE;
+        playerRunTexCoords[i].y = 3 * PLAYER_SIZE;
+      }
+    }
+    playerAnims[PlayerAnim::run] =
+        Frames(16, 2.0f, playerRunTexCoords, PLAYER_SIZE, PLAYER_SIZE);
+    playerAnims[PlayerAnim::jump] = Frames(
+        glm::vec2(2 * PLAYER_SIZE, 5 * PLAYER_SIZE), PLAYER_SIZE, PLAYER_SIZE);
+
+    player.pos = glm::vec2(0, sdlState.logHeight - 64);
+    player.tex = resourceManager.getPlayerTex();
+    player.maxSpeedX = 100.0f;
+    player.jumpAccel = -125.0f;
+    player.w = PLAYER_SIZE;
+    player.h = PLAYER_SIZE;
+    player.accel = glm::vec2(300, 0);
+    player.anims = playerAnims;
+    player.currAnim = PlayerAnim::idle;
+    player.collider.x = 12.0f;
+    player.collider.y = 18.0f;
+    player.collider.w = 8.0f;
+    player.collider.h = 10.0f;
+  }
+
+  static inline void loadTileMap(const SDLState &sdlState,
+                                 const ResourceManager &resourceManager) {
+    for (uint16_t r = 1; r <= 2; r++) {
+      for (uint16_t c = 0; c < MAP_COLS; c++) {
+        map[MAP_ROWS - r][c] = Tiles::DIRT;
+      }
+    }
+
+    staticTiles.reserve(100);
+    dynTiles.reserve(100);
+
+    for (uint16_t r = 0; r < MAP_ROWS; r++) {
+      for (uint16_t c = 0; c < MAP_COLS; c++) {
+        switch (map[r][c]) {
+          case Tiles::DIRT: {
+            StaticTile staticTile{};
+            staticTile.pos = glm::vec2(
+                c * TILE_SIZE, sdlState.logHeight - (MAP_ROWS - r) * TILE_SIZE);
+            staticTile.tex = resourceManager.getWorldTex();
+            staticTile.w = TILE_SIZE;
+            staticTile.h = TILE_SIZE;
+            staticTile.collider.x = 0;
+            staticTile.collider.y = 0;
+            staticTile.collider.w = staticTile.w;
+            staticTile.collider.h = staticTile.h;
+            staticTile.anims = std::vector<Frames>{
+                Frames(glm::vec2(0, TILE_SIZE), TILE_SIZE, TILE_SIZE)};
+            staticTiles.push_back(staticTile);
+            break;
+          }
+          case 0:
+            break;
+          default:
+            assert(false && "Unreachable");
+        }
+      }
+    }
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,6 @@
 #include <imgui.h>
 #include <imgui_impl_sdlrenderer3.h>
 
-void createPlayer(const SDLState &, const ResourceManager &);
-void loadTileMap(const SDLState &, const ResourceManager &);
-
 int main(int argc, char **argv) {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error",
@@ -35,8 +32,8 @@ int main(int argc, char **argv) {
   SDLState sdlState{"learn_sdl3", SDL_WINDOW_RESIZABLE, nullptr};
   ResourceManager resourceManager{sdlState};
 
-  createPlayer(sdlState, resourceManager);
-  loadTileMap(sdlState, resourceManager);
+  Game::createPlayer(sdlState, resourceManager);
+  Game::loadTileMap(sdlState, resourceManager);
 
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
@@ -143,90 +140,4 @@ int main(int argc, char **argv) {
   }
 
   return 0;
-}
-
-void createPlayer(const SDLState &sdlState,
-                  const ResourceManager &resourceManager) {
-  constexpr size_t PLAYER_IDLE_FRAMES = 4;
-  constexpr size_t PLAYER_RUN_FRAMES = 16;
-  constexpr float PLAYER_SIZE = 32.0f;
-
-  std::vector<Frames> playerAnims;
-  playerAnims.resize(5);
-
-  playerAnims[PlayerAnim::idle] = Frames(
-      PLAYER_IDLE_FRAMES, 0.5f,
-      std::vector<glm::vec2>{
-          glm::vec2(0 * PLAYER_SIZE, 0.0f), glm::vec2(1 * PLAYER_SIZE, 0.0f),
-          glm::vec2(2 * PLAYER_SIZE, 0.0f), glm::vec2(3 * PLAYER_SIZE, 0.0f)},
-      PLAYER_SIZE, PLAYER_SIZE);
-
-  std::vector<glm::vec2> playerRunTexCoords{PLAYER_RUN_FRAMES};
-  for (size_t i = 0; i < PLAYER_RUN_FRAMES; i++) {
-    if (i <= 7) {
-      playerRunTexCoords[i].x = i * PLAYER_SIZE;
-      playerRunTexCoords[i].y = 2 * PLAYER_SIZE;
-    } else {
-      playerRunTexCoords[i].x = (i - 8) * PLAYER_SIZE;
-      playerRunTexCoords[i].y = 3 * PLAYER_SIZE;
-    }
-  }
-  playerAnims[PlayerAnim::run] =
-      Frames(16, 2.0f, playerRunTexCoords, PLAYER_SIZE, PLAYER_SIZE);
-  playerAnims[PlayerAnim::jump] = Frames(
-      glm::vec2(2 * PLAYER_SIZE, 5 * PLAYER_SIZE), PLAYER_SIZE, PLAYER_SIZE);
-
-  Game::player.pos = glm::vec2(0, sdlState.logHeight - 64);
-  Game::player.tex = resourceManager.getPlayerTex();
-  Game::player.maxSpeedX = 100.0f;
-  Game::player.jumpAccel = -125.0f;
-  Game::player.w = PLAYER_SIZE;
-  Game::player.h = PLAYER_SIZE;
-  Game::player.accel = glm::vec2(300, 0);
-  Game::player.anims = playerAnims;
-  Game::player.currAnim = PlayerAnim::idle;
-  Game::player.collider.x = 12.0f;
-  Game::player.collider.y = 18.0f;
-  Game::player.collider.w = 8.0f;
-  Game::player.collider.h = 10.0f;
-}
-
-void loadTileMap(const SDLState &sdlState,
-                 const ResourceManager &resourceManager) {
-  for (uint16_t r = 1; r <= 2; r++) {
-    for (uint16_t c = 0; c < Game::MAP_COLS; c++) {
-      Game::map[Game::MAP_ROWS - r][c] = Tiles::DIRT;
-    }
-  }
-
-  Game::staticTiles.reserve(100);
-  Game::dynTiles.reserve(100);
-
-  for (uint16_t r = 0; r < Game::MAP_ROWS; r++) {
-    for (uint16_t c = 0; c < Game::MAP_COLS; c++) {
-      switch (Game::map[r][c]) {
-        case Tiles::DIRT: {
-          StaticTile staticTile{};
-          staticTile.pos = glm::vec2(
-              c * Game::TILE_SIZE,
-              sdlState.logHeight - (Game::MAP_ROWS - r) * Game::TILE_SIZE);
-          staticTile.tex = resourceManager.getWorldTex();
-          staticTile.w = Game::TILE_SIZE;
-          staticTile.h = Game::TILE_SIZE;
-          staticTile.collider.x = 0;
-          staticTile.collider.y = 0;
-          staticTile.collider.w = staticTile.w;
-          staticTile.collider.h = staticTile.h;
-          staticTile.anims = std::vector<Frames>{Frames(
-              glm::vec2(0, Game::TILE_SIZE), Game::TILE_SIZE, Game::TILE_SIZE)};
-          Game::staticTiles.push_back(staticTile);
-          break;
-        }
-        case 0:
-          break;
-        default:
-          assert(false && "Unreachable");
-      }
-    }
-  }
 }
