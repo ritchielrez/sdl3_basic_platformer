b7af8d01f371d45444a3a720792721872b0d78de
RitchielRez <ritchielrez@gmail.com>
2026-02-26 21:52:29 -0500

    fix(Log): `debug()` now relies on `Game::debug` not `DEBUG`

--- include/Log.h
+++ include/Log.h
@@ -2,12 +2,16 @@
 
 #include <fmt/color.h>
 
+#include "Game.h"
+
 namespace Log {
+// TODO: Maybe move this to UI, so instead of logging to console it will log in
+// the in game UI.
 template <typename... T>
 void debug(fmt::format_string<T...> fmt, T &&...args) {
-#ifdef DEBUG
-  fmt::print(fg(fmt::color::yellow), "[DEBUG] ");
-  fmt::print(fg(fmt::color::yellow), fmt, std::forward<T>(args)...);
-#endif
+  if (Game::debug) {
+    fmt::print(fg(fmt::color::yellow), "[DEBUG] ");
+    fmt::print(fg(fmt::color::yellow), fmt, std::forward<T>(args)...);
+  }
 }
 }  // namespace Log
