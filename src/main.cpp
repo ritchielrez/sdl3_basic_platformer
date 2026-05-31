00c1d319ad99c257bf9c34a20f6ec02561daddb6
RitchielRez <ritchielrez@gmail.com>
2026-05-31 11:26:36 -0400

    feat(main): change window title name to "Knightfall"

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
   // Create `Game` object which initializes game entities.
-  Game game{"sdl3_basic_platformer", SDL_WINDOW_RESIZABLE, nullptr};
+  Game game{"Knightfall", SDL_WINDOW_RESIZABLE, nullptr};
   // Then run the game.
   game.run();
   return 0;
